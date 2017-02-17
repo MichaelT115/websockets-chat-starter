773514c03b99c41cb5ef4ebbe04d21344e5c6c77
Michael Cooper <mtc5284@rit.edu>
2017-02-17 01:44:03 -0500

    Working with 1 extra thing

---
+++ src/responses.js
@@ -0,0 +1,33 @@
+const fs = require('fs');       // File system module
+const path = require('path');   // Path module
+
+// Maps extensions to content-types
+const extToType = {
+  '.mp4': 'video/mp4',
+  '.mp3': 'audio/mpeg',
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.js': 'text/babel',
+  '.xml': 'text/xml',
+};
+
+
+const getFile = (request, response, fileName) => {
+  const filePath = path.resolve(`${__dirname}/../client/`, `./${fileName}`);
+  const fileExt = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    // Handle error
+    if (err) {
+      return;
+    }
+
+    // Sends back the data.
+    response.writeHead(200, { 'Content-Type': extToType[fileExt] });
+    response.write(data);
+    response.end();
+  });
+};
+
+module.exports.getFile = getFile;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
 const http = require('http');
-const fs = require('fs');
+const url = require('url');
+
+const responses = require('./responses');
+
 const socketio = require('socket.io');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// read the client html file into memory.
-// __dirname in node is the current directory.
-// (In this case, the same folder as the server.js file)
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-
 const onRequest = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
-  response.end();
+  console.log(request.url);
+  const urlObject = url.parse(request.url, true); // This object represents the URL.
+
+
+  // This gets the title of the URL minus the query field.
+  switch (urlObject.pathname) {
+    // Default page
+    case '/':
+      responses.getFile(request, response, '/client.html');
+      break;
+    // Retrieves all other files. Also handles Resource Not Found conditions.
+    default:
+      responses.getFile(request, response, urlObject.pathname);
+      break;
+  }
 };
 
 const app = http.createServer(onRequest).listen(port);
@@ -25,22 +35,25 @@ const io = socketio(app);
 // Object to hold all of our connect users.
 const users = {};
 
+// Adds the events for when the user joins
 const onJoined = (sock) => {
   const socket = sock;
 
+  // On event "join"
   socket.on('join', (data) => {
-        // Message back to new user.
+    // Message back to new user.
     const joinMsg = {
       name: 'server',
       msg: `There are ${Object.keys(users).length} users online.`,
     };
-
     socket.name = data.name;
     socket.emit('msg', joinMsg);
 
     socket.join('room1');
 
-        // Announcement to everyone in the room1
+    users[data.name] = data.name;
+
+    // Announcement to everyone in the room1
     const response = {
       name: 'server',
       msg: `${data.name} has joined the room.`,
@@ -48,21 +61,37 @@ const onJoined = (sock) => {
     socket.broadcast.to('room1').emit('msg', response);
 
     console.log(`${data.name} joined`);
-        // Success message back to new user
+    // Success message back to new user
     socket.emit('msg', { name: 'server', msg: 'You joined the room' });
   });
 };
 
+// On receiving a message
 const onMsg = (sock) => {
   const socket = sock;
-
   socket.on('msgToServer', (data) => {
-    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg });
+    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg, color: data.color});
   });
 };
 
-const onDisconnet = (sock) => {
+// On receiving a 'disconnect' event
+const onDisconnect = (sock) => {
   const socket = sock;
+
+  socket.on('disconnect', (data) => {
+    console.dir(data);
+
+    const disconnectMsg = {
+      name: 'server',
+      msg: `${socket.name} has left the room.`,
+    };
+    socket.broadcast.to('room1').emit('msg', disconnectMsg);
+
+    socket.leave('room1');
+
+    // Delete user from list
+    delete users[socket.name];
+  });
 };
 
 io.sockets.on('connection', (socket) => {
@@ -70,7 +99,7 @@ io.sockets.on('connection', (socket) => {
 
   onJoined(socket);
   onMsg(socket);
-  onDisconnet(socket);
+  onDisconnect(socket);
 });
 
 console.log('Websocket server started');
